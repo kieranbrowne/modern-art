5b5798b4da6f01adf9bc712ada106c31ab14fe30
Kieran Browne <kierantbrowne@gmail.com>
2019-07-26 14:09:44 +1000

    Ben Nicholson OM; Foxy and Frankie variation 1

--- live.glsl
+++ live.glsl
@@ -1,11 +1,10 @@
 #define S(a,b,x) smoothstep(a,b,x)
-#define PI 3.14159265359
 mat2 rotate(float theta) {
   return mat2(cos(theta), -sin(theta),sin(theta),cos(theta));
 }
 
 // pixel pos
-vec2 uv = (gl_FragCoord.xy-.5*iResolution.xy) / iResolution.y * 2.;
+vec2 uv;
 // background color
 vec3 c = vec3(.99,.9,.8);
 
@@ -112,26 +111,11 @@ float sdBox( in vec2 p, in vec2 b )
   vec2 d = abs(p)-b;
   return length(max(d,vec2(0))) + min(max(d.x,d.y),0.0);
 }
-
-float sdLine( in vec2 p, in vec2 a, in vec2 b )
+float line( in vec2 p, in vec2 a, in vec2 b )
 {
   vec2 pa = p-a, ba = b-a;
   float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
   return length( pa - ba*h );
-}
-
-float sdTriangle( in vec2 p, in vec2 p0, in vec2 p1, in vec2 p2 )
-{
-  vec2 e0 = p1-p0, e1 = p2-p1, e2 = p0-p2;
-  vec2 v0 = p -p0, v1 = p -p1, v2 = p -p2;
-  vec2 pq0 = v0 - e0*clamp( dot(v0,e0)/dot(e0,e0), 0.0, 1.0 );
-  vec2 pq1 = v1 - e1*clamp( dot(v1,e1)/dot(e1,e1), 0.0, 1.0 );
-  vec2 pq2 = v2 - e2*clamp( dot(v2,e2)/dot(e2,e2), 0.0, 1.0 );
-  float s = sign( e0.x*e2.y - e0.y*e2.x );
-  vec2 d = min(min(vec2(dot(pq0,pq0), s*(v0.x*e0.y-v0.y*e0.x)),
-                   vec2(dot(pq1,pq1), s*(v1.x*e1.y-v1.y*e1.x))),
-               vec2(dot(pq2,pq2), s*(v2.x*e2.y-v2.y*e2.x)));
-  return -sqrt(d.x)*sign(d.y);
 }
 
 
@@ -139,112 +123,49 @@ void draw(vec3 color, float norm) {
   c = mix(c, color, max(0.,norm));
 }
 
-vec2 hash( vec2 p ) { p=vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))); return fract(sin(p)*18.5453); }
-
-vec3 voronoi( in vec2 x ) {
-    vec2 n = floor(x);
-    vec2 f = fract(x);
-
-    //----------------------------------
-    // first pass: regular voronoi
-    //----------------------------------
-	vec2 mg, mr;
-
-    float md = 8.0;
-    for( int j=-1; j<=1; j++ )
-    for( int i=-1; i<=1; i++ )
-    {
-        vec2 g = vec2(float(i),float(j));
-        vec2 o = hash( n + g );
-        vec2 r = g + o - f;
-        float d = dot(r,r);
-
-        if( d<md )
-        {
-            md = d;
-            mr = r;
-            mg = g;
-        }
-    }
-
-
-
-    //----------------------------------
-    // second pass: distance to borders
-    //----------------------------------
-    md = 8.0;
-    vec2 o;
-    for( int j=-2; j<=2; j++ )
-    for( int i=-2; i<=2; i++ )
-    {
-        vec2 g = mg + vec2(float(i),float(j));
-		    o = hash( n + g );
-        vec2 r = g + o - f;
-
-        if( dot(mr-r,mr-r)>0.00001 )
-        md = min( md, dot( 0.5*(mr+r), normalize(r-mr) ) );
-
-
-    }
-
-    return vec3( md, o);
-}
-
-vec3 palette[6];
-vec3 palette2[6];
-
+vec2 ps[12];
 
 
 void main () {
+  uv = (gl_FragCoord.xy-.5*iResolution.xy) / iResolution.y * 2.;
 
-  vec2 suv = uv;
+  uv += cnoise(uv*20.)/790.;
+  uv += cnoise(uv*60.)/790.;
+  uv += cnoise(uv*120.)/1990.;
+  // uv += cnoise(uv*10.)/190.;
 
+  draw(vec3(.0)*1.1,smoothstep(.0,0.,length(uv)));
 
-  palette[0] = vec3(255.,195.,055.)/256.;
-  palette[1] = vec3(230.,105.,00.)/256.;
-  palette[2] = vec3(230.,45.,50.)/256.;
-  palette[3] = vec3(180.,20.,20.)/256.;
-  palette[4] = vec3(80.,00.,050.)/256.;
-  palette[5] = vec3(20.,40.,120.)/256.;
+  int i = 0;
+  ps[i++] = vec2(.14,.56);
+  ps[i++] = vec2(-.20,.50);
+  ps[i++] = vec2(.15,.87);
+  ps[i++] = vec2(.15,.47);
+  ps[i++] = vec2(0.,0.);
+  ps[i++] = vec2(.3,-.8);
+  ps[i++] = vec2(.1,-.78);
+  ps[i++] = vec2(.35,.2);
+  ps[i++] = vec2(-.5,.5);
+  ps[i++] = vec2(-.25,-.4);
+  ps[i++] = vec2(.52,.6);
+  ps[i++] = vec2(.43,-.3);
+  ps[i++] = vec2(.25,-.22);
 
-  palette2[0] = palette[0];
-  palette2[1] = vec3(100.,165.,40.)/256.;
-  palette2[2] = vec3(80.,145.,80.)/256.;
-  palette2[3] = vec3(50.,100.,100.)/256.;
-  palette2[4] = vec3(30.,80.,140.)/256.;
-  palette2[5] = palette[5];
+  float v = 0.;
 
-  draw(vec3(.75,.55,.4)*.9,smoothstep(.0,0.,length(uv)));
-  c += cnoise(uv*200.)/60.;
-  c += cnoise(uv*320.)/40.;
-  c += cnoise(uv*280.)/50.;
+  // eyes
+  v += S(.013,.006,abs(.03- length(uv+vec2(-.07,-.68))));
+  // v += S(.013,.006,abs(.03- length(uv+vec2(.05,.41))));
 
-  int idx = 0;
-
-
-  for(float i=.8; i>= 0.0; i-= 1/7.4) {
-    draw(palette[idx], S(0.004 +i,.00+i,length(uv))
-         // *S(.1,.2,length(uv+.2))
-
-         );
-
-    draw(palette2[5-idx], S(0.004 +i,.00+i,length(uv*rotate(11.8)+uv.y/8.))
-         * (
-            S(.105,.1,sdBox((uv*rotate(3.1415*.5)+vec2(.0,0.84))*rotate(3.1415*.25),vec2(.5)))
-            +S(.105,.1,sdBox((uv*rotate(PI*.5)+vec2(.0,-0.84))*rotate(3.1415*.25),vec2(.5)))
-            )
-
-         );
-
-    // break;
-    idx ++;
+  // lines
+  for(int i=0; i<ps.length(); i++) {
+    v += S(.015,.01,line(uv,ps[i],ps[i+1]));
   }
 
+  // border
+  v += S(.9,.904,ngon(uv, vec2(0.),4));
 
-  c += cnoise(uv*230.)/60.;
-  c += cnoise(uv*350.)/40.;
-  c += cnoise(uv*290.)/50.;
-
+  draw(vec3(.9,.87,.78), min(1., v));
 
   gl_FragColor = vec4(c, 1.);
 }
